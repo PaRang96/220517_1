cf05316c13e2a922be1d142c50929726fab284c5
PaRang96 <winterreies94@gmail.com>
2022-05-17 09:47:09 +0900

    monster -> move, drop, attack function

--- Monster.cpp
+++ Monster.cpp
@@ -12,3 +12,15 @@ int Monster::Death(PlayerCharacter& PlayerCharacter, int Gold)
 {
     return 0;
 }
+
+void Monster::Move()
+{
+}
+
+void Monster::Drop()
+{
+}
+
+void Monster::Attack()
+{
+}
--- Monster.h
+++ Monster.h
@@ -9,10 +9,14 @@ private:
 	string MonsterName;
 	int Hp;
 	int Gold;
+	int Damage;
 
 public:
 	Monster();
 	~Monster();
 	int Death(PlayerCharacter& PlayerCharacter, int Gold);
+	void Move();
+	void Drop();
+	void Attack();
 };
 
