1621ff976871bcceac4833944be395405e3d85ad
PaRang96 <winterreies94@gmail.com>
2022-05-17 13:45:20 +0900

    edited creation process -> unified to vector<Monster*> Monsters

--- main.cpp
+++ main.cpp
@@ -16,40 +16,11 @@ int main()
 	srand(static_cast<unsigned int>(time(nullptr)));
 	
 	// random number of instances (1 ~ 3)
-	int GoblinSpawnCount = rand() % 3 + 1;
 	vector<Monster*> Monsters;
-	for (int i = 0; i < GoblinSpawnCount; i++)
-	{
-		Monsters.push_back(new Goblin);
-	}
-
-	int SlimeSpawnCount = rand() % 3 + 1;
-	for (int i = 0; i < SlimeSpawnCount; i++)
-	{
-		Monsters.push_back(new Slime);
-	}
-
-	int BoarSpawnCount = rand() % 3 + 1;
-	for (int i = 0; i < BoarSpawnCount; i++)
-	{
-		Monsters.push_back(new Boar);
-	}
-
-	cout << '\n' << "------------------" << '\n' << endl;
-
-	// movement of the created monsters -> Tick in UE
-	for (int i = 0; i < Monsters.size(); i++)
-	{
-		Monsters[i]->Move();
-	}
-
-	cout << '\n' << "------------------" << '\n' << endl;
-
-	// destroy the created monsters
-	for (int i = 0; i < Monsters.size(); i++)
-	{
-		delete Monsters[i];
-	}
+	Monsters.push_back(new Goblin);
+	Monsters.push_back(new Slime);
+	Monsters.push_back(new Boar);
+	Monsters.push_back(new Boar);
 
 	delete MyPlayer;
 	MyPlayer = nullptr;
